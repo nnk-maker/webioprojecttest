729a7646f9115687aa0609edf64625750d1f3aec
nnk-maker <krishnanarasimha4@gmail.com>
2022-06-28 21:55:38 +0200

    dynamicloading page
    Installing allure:
    npm i allure-commandline -g
    
    this to generate report : allure generate allure-results && allure open.

--- test/specs/dynamic-loading.e2e.js
+++ test/specs/dynamic-loading.e2e.js
@@ -2,19 +2,20 @@ describe('Dynamic Loading Page', () => {
     it('wait for the hidden element to show',async () => {
             const btnStart = await $('#start button')
             const textFinish = await $('#finish h4')
-           // const elLoading = $('#loading');
-            await browser.url('/dynamic_loading/1')
+            const elLoading = await $('#loading');
+            browser.url('/dynamic_loading/1')
 
-            await btnStart.click();
+            btnStart.click();
 
             
-            const text = await textFinish.getText()
-            await textFinish.waitForDisplayed()
+           // const text = await textFinish.getText()
+           //await textFinish.waitForDisplayed()
 
             //const tFinish = await textFinish.getText()
-
+            elLoading.waitForDisplayed()
+            elLoading.waitForDisplayed({reverse:true})
             //assert(tFinish === 'Hello World!')
-            await expect(textFinish.getText()).toEqual("Hello World!")
+           await expect(textFinish.getText()).toEqual("Hello World!");
 
             
     });
