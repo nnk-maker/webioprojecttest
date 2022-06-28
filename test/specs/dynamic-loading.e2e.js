686ca7730b92a26d8b86a394e98fee3cf2ce8343
nnk-maker <krishnanarasimha4@gmail.com>
2022-06-28 16:51:30 +0200

    dynamicloading
    npx wdio --spec ./test\specs\dynamic-loading.e2e.js

---
+++ test/specs/dynamic-loading.e2e.js
@@ -0,0 +1,21 @@
+describe('Dynamic Loading Page', () => {
+    it('wait for the hidden element to show',async () => {
+            const btnStart = await $('#start button')
+            const textFinish = await $('#finish h4')
+           // const elLoading = $('#loading');
+            await browser.url('/dynamic_loading/1')
+
+            await btnStart.click();
+
+            
+            const text = await textFinish.getText()
+            await textFinish.waitForDisplayed()
+
+            //const tFinish = await textFinish.getText()
+
+            //assert(tFinish === 'Hello World!')
+            await expect(textFinish.getText()).toEqual("Hello World!")
+
+            
+    });
+})
